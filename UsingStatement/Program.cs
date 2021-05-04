b799d7f08ae60a633c41bf609d177867b456f92d
Vagner de Souza <inaciovagner1@gmail.com>
2021-05-04 13:37:18 +0100

    Add project files.

---
+++ UsingStatement/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UsingStatement
+{
+    class check_using : IDisposable
+    {
+        public void Dispose()
+        {
+            Console.WriteLine("Execute Second");
+        }
+        static void Main(string[] args)
+        {
+            using (check_using c = new check_using())
+            {
+                Console.WriteLine("Execute First");
+            }
+            Console.WriteLine("Execute Third");
+            Console.ReadLine();
+        }
+    }
+}
